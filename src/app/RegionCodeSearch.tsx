3c51692577f48b7e9ee2bec560e3d6603fafc635
Christien Guy <christien.guy@gmail.com>
2024-07-30 16:52:15 +0100

    feat: return a list of regions

--- src/app/RegionCodeSearch.tsx
+++ src/app/RegionCodeSearch.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { FormEvent, useState } from "react";
-import { findRegionCode } from "./actions";
+import { findRegion, findAllRegions, Region } from "./actions";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
@@ -10,17 +10,15 @@ import { Label } from "@/components/ui/label";
 export function RegionCodeSearch() {
   "use client";
   const [searchQuery, setSearchQuery] = useState("");
-  const [regionCode, setRegionCode] = useState("");
-  const [regionName, setRegionName] = useState("");
+  const [allMatchingRegions, setAllMatchingRegions] = useState<Array<Region>>(
+    []
+  );
 
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const region = await findRegionCode(searchQuery);
-    setRegionCode(region.code);
-    setRegionName(region.name);
+    const regions = await findAllRegions(searchQuery);
+    setAllMatchingRegions(regions);
   };
-
-  const regionPath = regionCode.toLowerCase().split("-").join("/");
 
   return (
     <div className="flex flex-col gap-4">
@@ -36,15 +34,20 @@ export function RegionCodeSearch() {
         />
         <Button type="submit">Search for Region</Button>
       </form>
-      {regionName && (
-        <Link
-          // TODO: abstract this into an Anchor component to share styles
-          className="font-medium text-primary underline underline-offset-2"
-          href={`/recent/${regionPath}`}
-        >
-          View recent sightings in {regionName}
-        </Link>
-      )}
+      {allMatchingRegions.length > 0 &&
+        allMatchingRegions.map((region) => {
+          const regionPath = region.code.toLowerCase().split("-").join("/");
+          return (
+            <Link
+              key={region.code}
+              // TODO: abstract this into an Anchor component to share styles
+              className="font-medium text-primary underline underline-offset-2"
+              href={`/recent/${regionPath}`}
+            >
+              View recent sightings in {region.name}
+            </Link>
+          );
+        })}
     </div>
   );
 }
--- src/app/actions.ts
+++ src/app/actions.ts
@@ -2,11 +2,11 @@
 
 import { regionCodes } from "@/regionCodes";
 
-type RegionCode = {
+export type Region = {
   name: string;
   code: string;
 };
-type RegionCodes = Array<{
+export type RegionTree = Array<{
   code: string;
   name: string;
   subnational1?: Array<{
@@ -19,12 +19,44 @@ type RegionCodes = Array<{
   }>;
 }>;
 
+// Search through the regionCodes array and subnational1 and subnational2 arrays
+// find where the region name includes the query
+// Add that region code to the result array
+// return the result array
+function findAllRegionsByQuery(query: string): Region[] | undefined {
+  const queryLower = query.toLowerCase();
+  const results: Region[] = [];
+  for (const region of regionCodes) {
+    if (region.name.toLowerCase().includes(queryLower)) {
+      results.push({ name: region.name, code: region.code });
+    }
+
+    if (region.subnational1) {
+      for (const sub1 of region.subnational1) {
+        if (sub1.name.toLowerCase().includes(queryLower)) {
+          results.push({ name: sub1.name, code: sub1.code });
+        }
+
+        if (sub1.subnational2) {
+          for (const sub2 of sub1.subnational2) {
+            if (sub2.name.toLowerCase().includes(queryLower)) {
+              results.push({ name: sub2.name, code: sub2.code });
+            }
+          }
+        }
+      }
+    }
+  }
+
+  return results;
+}
+
 // Recursively search through the regionCodes array
 // to find the region code where the query matches the region name
-function recursivelyFindRegionCodeByQuery(
+function recursivelyFindRegionByQuery(
   query: string,
-  list: RegionCodes = regionCodes
-): RegionCode | undefined {
+  list: RegionTree = regionCodes
+): Region | undefined {
   const queryLower = query.toLowerCase();
   for (const region of list) {
     if (region.name.toLowerCase().includes(queryLower)) {
@@ -32,7 +64,7 @@ function recursivelyFindRegionCodeByQuery(
     }
 
     if (region.subnational1) {
-      const subnational1RegionCode = recursivelyFindRegionCodeByQuery(
+      const subnational1RegionCode = recursivelyFindRegionByQuery(
         queryLower,
         region.subnational1
       );
@@ -42,7 +74,7 @@ function recursivelyFindRegionCodeByQuery(
 
       for (const sub1 of region.subnational1) {
         if (sub1.subnational2) {
-          const subnational2RegionCode = recursivelyFindRegionCodeByQuery(
+          const subnational2RegionCode = recursivelyFindRegionByQuery(
             queryLower,
             sub1.subnational2
           );
@@ -55,10 +87,18 @@ function recursivelyFindRegionCodeByQuery(
   }
 }
 
-export async function findRegionCode(query: string): Promise<RegionCode> {
-  const regionCode = recursivelyFindRegionCodeByQuery(query);
+export async function findRegion(query: string): Promise<Region> {
+  const regionCode = recursivelyFindRegionByQuery(query);
   if (!regionCode) {
     throw new Error(`Could not find region code for ${query}`);
   }
   return regionCode;
+}
+
+export async function findAllRegions(query: string): Promise<Region[]> {
+  const regions = findAllRegionsByQuery(query);
+  if (!regions || regions.length === 0) {
+    throw new Error(`Could not find any regions matching ${query}`);
+  }
+  return regions;
 }
