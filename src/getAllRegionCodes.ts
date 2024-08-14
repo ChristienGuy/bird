04f33a6e051558311859a440b076f9248816ee9a
Christien Guy <christien.guy@gmail.com>
2024-08-14 18:50:54 +0100

    fix: remove unused function and rename

--- src/getAllRegionCodes.ts
+++ src/getAllRegionCodes.ts
@@ -1,3 +1,4 @@
+import { Region } from "./app/actions";
 import { EBIRD_BASE_API_URL } from "./constants";
 
 const getCountries = async (): Promise<
@@ -65,72 +66,7 @@ async function getSubNational2(region: string): Promise<
   return response.json();
 }
 
-[
-  {
-    name: "United States",
-    code: "US",
-    subnational1: [
-      {
-        name: "California",
-        code: "US-CA",
-        subnational2: [
-          {
-            name: "San Francisco",
-            code: "US-CA-075",
-          },
-          {
-            name: "Los Angeles",
-            code: "US-CA-037",
-          },
-        ],
-      },
-    ],
-  },
-];
-
-async function getAllSubnationals(countryCode: string) {
-  const countries = await getCountries();
-  const country = countries.find((c) => c.code === countryCode);
-
-  if (!country) {
-    throw new Error(`Country with code ${countryCode} not found`);
-  }
-
-  const subnational1 = await getSubnational1(country?.code);
-  const subnational2 = await getSubNational2(country?.code);
-
-  const countryWithNestedSubnationals = {
-    name: country?.name,
-    code: country?.code,
-    subnational1: subnational1.map((sub1) => {
-      const sub2s = subnational2.filter((sub2) => {
-        const parentCode = sub2.code.split("-").slice(0, -1).join("-");
-        return sub1.code === parentCode;
-      });
-      return {
-        ...sub1,
-        subnational2: sub2s,
-      };
-    }),
-  };
-
-  return countryWithNestedSubnationals;
-}
-
-export async function getAllCountriesWithSubnationals(): Promise<
-  Array<{
-    name: string;
-    code: string;
-    subnational1: Array<{
-      name: string;
-      code: string;
-      subnational2: Array<{
-        name: string;
-        code: string;
-      }>;
-    }>;
-  }>
-> {
+export async function getNestedRegionsData(): Promise<Array<Region>> {
   const countries = await getCountries();
   const countriesWithSubnationals = await Promise.all(
     countries.map(async (country) => {
