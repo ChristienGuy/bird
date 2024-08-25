5130ab4d5e0146d9651acef3f004c24467fd7705
Christien Guy <christien.guy@gmail.com>
2024-08-25 17:23:07 +0100

    feat: evenly scatter lat lng points to avoid overlap

--- src/app/recent/nearby/map-nearby.tsx
+++ src/app/recent/nearby/map-nearby.tsx
@@ -22,26 +22,26 @@ function deterministicallyDistributeLatLng(
   index: number,
   total = 10,
 ) {
-  console.log("sighting", {
-    sciName: sighting.sciName,
-  });
-
-  // Calculating the angle from 0 to 2PI (360 degrees)
+  // Calculating the angle for the current slice
   // based on cutting the circle into {total} equal parts
   // and then multiplying by the index to get the angle
   const angle = (index / total) * Math.PI * 2;
 
-  const latNoise = normalise(sighting.sciName.charCodeAt(0));
-  const lngNoise = normalise(sighting.sciName.charCodeAt(1));
+  // We use the first two characters of the scientific name
+  // to get a hard to predict but deterministic value
+  // This gets us a value between 65 and 122 (charCodes for [A-Za-z])
+  // Which we then normalise to a value between 0 and 1
+  // We then multiply this by a small value to get a small distance offset
+  // to later modify the lat/lng (very small numbers equate to 100s of meters)
+  const latDistance = normalise(sighting.sciName.charCodeAt(1)) * 0.0025;
+  const lngDistance = normalise(sighting.sciName.charCodeAt(2)) * 0.0025;
 
-  console.log("noise", {
-    latNoise,
-    lngNoise,
-  });
-
+  // We then calculate the new lat/lng by offsetting the original lat/lng
+  // using both the angle and the distance so that all the points
+  // are distributed around the original point in a circle by "random" distances
   return {
-    lat: sighting.lat + Math.sin(angle) * latNoise * 0.006,
-    lng: sighting.lng + Math.cos(angle) * lngNoise * 0.006,
+    lat: sighting.lat + Math.sin(angle) * latDistance,
+    lng: sighting.lng + Math.cos(angle) * lngDistance,
   };
 }
 
