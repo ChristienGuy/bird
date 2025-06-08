e1ef7630c57e32616908eeecabad25660df4844b
Daniel Elvey <delvey21@hotmail.co.uk>
2025-06-08 10:09:03 +0100

    replaced hardcoded hex bg value with tailwind colour. Centred region search heading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { TopNav } from "./components/top-nav";
+import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className} style={{ backgroundColor: "#eee8e4" }}>
+      <body className={cn(`bg-orange-50`, inter.className)}>
         <main>
           <div className="grid min-h-dvh grid-rows-[auto_1fr]">
             <TopNav />
--- src/app/recent/region/page.tsx
+++ src/app/recent/region/page.tsx
@@ -2,14 +2,14 @@ import { RegionCodeSearch } from "@/app/RegionCodeSearch";
 
 export default function Page() {
   return (
-    <>
-      <h1 className="mb-12 text-4xl">
+    <div className="flex flex-col items-center">
+      <h1 className="mb-12 mt-4 text-4xl">
         Recent bird sightings by Region{" "}
         <span role="img" aria-label="bird">
           🐦
         </span>
       </h1>
       <RegionCodeSearch />
-    </>
+    </div>
   );
 }
