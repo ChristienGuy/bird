677d25f6d60199b99e37f5c3c0f32b270c5e1dd7
Daniel Elvey <delvey21@hotmail.co.uk>
2025-06-11 16:58:55 +0100

    restructured nearby page to be a client component and use stored coordinates if available

--- src/app/contexts/user-coordinates-provider.tsx
+++ src/app/contexts/user-coordinates-provider.tsx
@@ -61,6 +61,10 @@ function UserCoordinatesProvider({
         try {
           const parsed = JSON.parse(savedCoordinates);
           setUserCoordinates(parsed);
+          console.log(
+            "Location coordinates from cookies successfully retrieved:",
+            parsed,
+          );
         } catch (error) {
           console.log("Failed to parse saved coordinates:", error);
         }
@@ -72,6 +76,10 @@ function UserCoordinatesProvider({
   useEffect(() => {
     if (userCoordinates.latitude !== 0 || userCoordinates.longitude !== 0) {
       setCookie(COOKIE_NAME, JSON.stringify(userCoordinates));
+      console.log(
+        "Location coordinates saved to cookies successfully:",
+        userCoordinates,
+      );
     }
   }, [userCoordinates]);
 
--- src/app/recent/nearby/page.tsx
+++ src/app/recent/nearby/page.tsx
@@ -1,13 +1,31 @@
-import { getNearbySightings } from "@/app/actions";
+"use client";
+
+import { getNearbySightings, NearbySightingsGetResponse } from "@/app/actions";
 import { MapNearby } from "./map-nearby";
 import { deterministicallyDistributeLatLng } from "./nearby-map-util";
+import { useCoordinates } from "../../contexts/user-coordinates-provider";
+import { useEffect, useState } from "react";
 
-export default async function NearbySightingsPage() {
-  const nearbySightingsResponse = await getNearbySightings({
-    latitude: 53.185335,
-    longitude: -1.688074,
-    distance: 10,
-  });
+export default function NearbySightingsPage() {
+  const [nearbySightingsResponse, setNearbySightingsResponse] =
+    useState<NearbySightingsGetResponse>();
+  const { userCoordinates } = useCoordinates();
+
+  useEffect(() => {
+    async function getSightingsResponse() {
+      const nearbyResponse = await getNearbySightings({
+        latitude: userCoordinates.latitude ?? 53.185335,
+        longitude: userCoordinates.longitude ?? -1.688074,
+        distance: 10,
+      });
+      setNearbySightingsResponse(nearbyResponse);
+    }
+    getSightingsResponse();
+  }, []);
+
+  if (!nearbySightingsResponse) {
+    return <h1>Loading...</h1>;
+  }
 
   const nearbySightings = nearbySightingsResponse.map((sighting, index) => {
     if (index === 0) return sighting;
