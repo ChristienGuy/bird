d31a575d05b570e3700ee433247e00419575a178
Daniel Elvey <delvey21@hotmail.co.uk>
2025-06-11 17:09:05 +0100

    reverted nearby page restructure as it crashes when user coordinates arent available - will revisit later

--- src/app/recent/nearby/page.tsx
+++ src/app/recent/nearby/page.tsx
@@ -1,31 +1,13 @@
-"use client";
-
-import { getNearbySightings, NearbySightingsGetResponse } from "@/app/actions";
+import { getNearbySightings } from "@/app/actions";
 import { MapNearby } from "./map-nearby";
 import { deterministicallyDistributeLatLng } from "./nearby-map-util";
-import { useCoordinates } from "../../contexts/user-coordinates-provider";
-import { useEffect, useState } from "react";
 
-export default function NearbySightingsPage() {
-  const [nearbySightingsResponse, setNearbySightingsResponse] =
-    useState<NearbySightingsGetResponse>();
-  const { userCoordinates } = useCoordinates();
-
-  useEffect(() => {
-    async function getSightingsResponse() {
-      const nearbyResponse = await getNearbySightings({
-        latitude: userCoordinates.latitude ?? 53.185335,
-        longitude: userCoordinates.longitude ?? -1.688074,
-        distance: 10,
-      });
-      setNearbySightingsResponse(nearbyResponse);
-    }
-    getSightingsResponse();
-  }, []);
-
-  if (!nearbySightingsResponse) {
-    return <h1>Loading...</h1>;
-  }
+export default async function NearbySightingsPage() {
+  const nearbySightingsResponse = await getNearbySightings({
+    latitude: 53.185335,
+    longitude: -1.688074,
+    distance: 10,
+  });
 
   const nearbySightings = nearbySightingsResponse.map((sighting, index) => {
     if (index === 0) return sighting;
